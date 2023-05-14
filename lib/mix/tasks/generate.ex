e331fd7f97e97bfe514a3924951c9dde8d3eda44
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-05-15 07:39:53 +1000

    fix(generate): Generate with the correct tag

--- lib/mix/tasks/generate.ex
+++ lib/mix/tasks/generate.ex
@@ -9,7 +9,7 @@ defmodule Mix.Tasks.Craft.Gen do
   @shortdoc "Generates a CHANGELOG.md file for the current tag"
   def run(_) do
     {:ok, version} = current_version()
-    {output, exit_code} = System.cmd("git-cliff", ["--tag", "#{version}", "--prepend", "./CHANGELOG.md", "-u"])
+    {output, exit_code} = System.cmd("git-cliff", ["--tag", "v#{version}", "--prepend", "./CHANGELOG.md", "-u"])
 
     if exit_code != 0 do
       raise "Git Cliff failed with a non-zero exit code\n\n#{output}"
